c25a8e011a997ed35006e1217ceda0206fe9f757
sstark21 <va.sabitov@gmail.com>
2019-01-09 13:28:30 +0300

    09.01

--- libft.h
+++ libft.h
@@ -17,12 +17,6 @@ int		ft_islower(int c);
 int		ft_isprint(int c);
 int		ft_isupper(int c);
 char	*ft_itoa(int n);
-//void	ft_lstadd(t_list **alst, t_list *new);
-//void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
-//void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
-//void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-//t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-//t_list	*ft_lstnew(void const *content, size_t content_size);
 char	*ft_memalloc(size_t size);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(char *c, int i, size_t j);
@@ -66,13 +60,17 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 
 
-
 typedef struct		s_list
 {
 	void			*content;
 	size_t 			content_size;
 	struct s_list	*next;
 }					t_list;
-
+void	ft_lstadd(t_list **alst, t_list *new);
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list	*ft_lstnew(void const *content, size_t content_size);
 
 #endif
