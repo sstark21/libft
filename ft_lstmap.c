d48c6104a60b1dae10cf0414ee84caee55f1b922
sstark21 <va.sabitov@gmail.com>
2019-01-09 13:23:48 +0300

    09.01

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,5 +14,28 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
+	t_list *first;
+	t_list *prev;
+	t_list *new;
 
+	first = NULL;
+	if (lst != NULL)
+	{
+		first = ft_lstnew(lst->content, lst->content_size);
+		if (first == NULL)
+			return (NULL);
+		first = f(first);
+		prev = first;
+		lst = lst->next;
+	}
+	while (lst != NULL)
+	{
+		new = ft_lstnew(lst->content, lst->content_size);
+		if (new == NULL)
+			return (NULL);
+		new = f(new);
+		prev->next = new;
+		lst = lst->next;
+	}
+	return (first);
 }
