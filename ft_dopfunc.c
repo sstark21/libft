a4713e10dc1b714da1c6e880f30d16440050696e
sstark21 <va.sabitov@gmail.com>
2019-01-13 15:02:30 +0300

    13.01

---
+++ ft_dopfunc.c
@@ -0,0 +1,4 @@
+int	ft_dopfunc(int one)
+{
+
+}
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: sstark <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/30 22:18:42 by sstark            #+#    #+#             */
-/*   Updated: 2018/12/30 22:18:44 by sstark           ###   ########.fr       */
+/*   Updated: 2019/01/13 14:46:19 by sstark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,28 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t hlen)
 {
-	size_t	nlen;
-	int		rez;
+	size_t	i;
+	size_t	j;
+	size_t	lenn;
+	size_t	lenh;
 
+	lenn = ft_strlen((char *)needle);
+	lenh = ft_strlen((char *)haystack);
 	if (*needle == '\0')
 		return ((char *)haystack);
-	nlen = ft_strlen((char *)needle);
-	rez = 1;
-	while (nlen <= hlen && *haystack &&
-	(rez = ft_strncmp(haystack, needle, nlen)))
-	{
-		hlen--;
-		haystack++;
-	}
-	if (rez != 0)
+	if (lenn > hlen || lenn > lenh)
 		return (NULL);
-	else
-		return ((char *)haystack);
+	i = 0;
+	while (i <= (hlen - lenn) && i < lenh)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
